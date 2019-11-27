8ea3bd15597daab8b7088154ff12af2d71b48931
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-11-27 14:06:26 +0100

    Basic specifications/interface and some methods for the message listener

---
+++ smslibrary/src/main/java/com/eis/smslibrary/ReceivedMessageListener.java
@@ -0,0 +1,17 @@
+package com.eis.smslibrary;
+
+import com.eis.communication.Message;
+
+/**
+ * Generic interface of a listener for incoming messages as per the
+ * <a href='https://refactoring.guru/design-patterns/observer'>Observer Design Pattern</a>
+ * @param <T> The type of message to receive
+ */
+public interface ReceivedMessageListener<T extends Message> {
+
+    /**
+     * Called by SMSHandler whenever a message is received.
+     * @param message The message received
+     */
+    void onMessageReceived(T message);
+}
--- smslibrary/src/main/java/com/eis/smslibrary/SMSHandler.java
+++ smslibrary/src/main/java/com/eis/smslibrary/SMSHandler.java
@@ -4,10 +4,40 @@ import com.eis.communication.CommunicationHandler;
 import com.eis.communication.Message;
 
 
-public class SMSHandler implements CommunicationHandler {
+public class SMSHandler implements CommunicationHandler<SMSMessage> {
 
+    private SMSHandler instance;
+    private ReceivedMessageListener<SMSMessage> listener;
+
+    /**
+     * Private constructor as per <a href='https://refactoring.guru/design-patterns/singleton'>Singleton Design Pattern</a>
+     */
+    private SMSHandler(){
+
+    }
+
+    /**
+     * Implementation of a <a href='https://refactoring.guru/design-patterns/singleton'>Singleton Design Pattern</a>
+     */
+    public SMSHandler getInstance(){
+        if(instance == null) instance = new SMSHandler();
+        return instance;
+    }
+
+    /**
+     * Sends a single message in the channel, message content must be shorter than the maximum channel message size
+     * @param message message to be sent in the channel to a peer
+     */
     @Override
-    public void sendMessage(Message message) {
+    public void sendMessage(SMSMessage message) {
         //Test pull request
+    }
+
+    /**
+     * Sets the listener observing for incoming SMSMessages
+     * @param listener The listener of type SMSMessage
+     */
+    public void addReceiveListener(ReceivedMessageListener<SMSMessage> listener){
+
     }
 }
