aac2322d6f753293955e9e2789f0ac71f657b71a
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-12-13 12:40:42 +0100

    Fully tested SMSDeliveredBroadcastReceiver

---
+++ smslibrary/src/test/java/com/eis/smslibrary/SMSDeliveredBroadcastReceiverTest.java
@@ -0,0 +1,67 @@
+package com.eis.smslibrary;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+
+import com.eis.smslibrary.listeners.SMSDeliveredListener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SMSDeliveredBroadcastReceiverTest {
+
+    @Mock SMSDeliveredListener listener;
+    private final SMSPeer VALID_PEER = new SMSPeer("+395554");
+    private final String TEXT_MESSAGE = "Sentence to test";
+    private final SMSMessage VALID_MESSAGE = new SMSMessage(VALID_PEER, TEXT_MESSAGE);
+    private Context contextMock = mock(Context.class);
+
+    @Test
+    public void testConstructor_noErrors(){
+        SMSDeliveredBroadcastReceiver receiver = new SMSDeliveredBroadcastReceiver(VALID_MESSAGE, listener);
+    }
+
+    @Test
+    public void receiveMessage_okResult(){
+        SMSDeliveredBroadcastReceiver rec = spy(new SMSDeliveredBroadcastReceiver(VALID_MESSAGE, listener));
+
+        doReturn(Activity.RESULT_OK).when((BroadcastReceiver)rec).getResultCode();
+        rec.onReceive(contextMock, null);
+    }
+
+    @Test
+    public void receiveMessage_CanceledResult(){
+        SMSDeliveredBroadcastReceiver rec = spy(new SMSDeliveredBroadcastReceiver(VALID_MESSAGE, listener));
+
+        doReturn(Activity.RESULT_CANCELED).when((BroadcastReceiver)rec).getResultCode();
+        rec.onReceive(contextMock, null);
+    }
+
+    @Test
+    public void receiveMessage_verifyResultOkParameters(){
+        SMSDeliveredBroadcastReceiver rec = spy(new SMSDeliveredBroadcastReceiver(VALID_MESSAGE, listener));
+        doReturn(Activity.RESULT_OK).when((BroadcastReceiver)rec).getResultCode();
+        rec.onReceive(contextMock, null);
+        verify(listener, times(1))
+                .onSMSDelivered(VALID_MESSAGE, SMSMessage.DeliveredState.MESSAGE_DELIVERED);
+    }
+
+    @Test
+    public void receiveMessage_verifyResultErrorParameters(){
+        SMSDeliveredBroadcastReceiver rec = spy(new SMSDeliveredBroadcastReceiver(VALID_MESSAGE, listener));
+        doReturn(Activity.RESULT_CANCELED).when((BroadcastReceiver)rec).getResultCode();
+        rec.onReceive(contextMock, null);
+        verify(listener, times(1))
+                .onSMSDelivered(VALID_MESSAGE, SMSMessage.DeliveredState.DELIVERY_ERROR);
+    }
+}
